a85b863b81d29f069ec8964e05e91e8346677fe7
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-21 06:27:29 +0600

    updateToy get

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oajesmx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,6 +41,20 @@ async function run() {
 
     app.get('/allToys', async(req, res)=>{
       const result = await wheelWhizCollection.find().limit(20).toArray();
+      res.send(result)
+    })
+
+    app.get('/myToys', async(req, res)=>{
+      const email = req.query.email;
+      const quary = {sellerEmail: {$eq: email}};
+      const result = await wheelWhizCollection.find(quary).toArray();
+      res.send(result)
+    })
+
+    app.get('/updateToy/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const result = await wheelWhizCollection.findOne(filter);
       res.send(result)
     })
 
